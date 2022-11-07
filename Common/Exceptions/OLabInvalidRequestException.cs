d4de1ab5361d7874fc6398c2c3c55cb45eda861d
Corey <reg@pathrider.ca>
2022-11-07 16:05:18 -0700

    Intermediate check-in

---
+++ Common/Exceptions/OLabInvalidRequestException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace OLabWebAPI.Common.Exceptions
+{
+    [ExcludeFromCodeCoverage]
+    public class OLabInvalidRequestException : Exception
+    {
+        public OLabInvalidRequestException(Exception exception)
+            : base("Invalid request", exception)
+        { }
+
+        public OLabInvalidRequestException(string message)
+            : base(message)
+        { }        
+    }
+}
