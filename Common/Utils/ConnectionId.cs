f832b6aaca881eae91b5af28401f4700e5ea19c5
Corey <reg@pathrider.ca>
2022-12-01 12:34:20 -0700

    Bidirectional messaging working

---
+++ Common/Utils/ConnectionId.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Utils
+{
+  public static class ConnectionId
+  {
+    public static string Shorten(string connectionId) 
+    {
+      if (string.IsNullOrEmpty(connectionId))
+        return "<none>";
+      return connectionId.Substring(connectionId.Length - 3); 
+    }
+  }
+}
