0e165373f7bb856199e5bf8fee86c6ec710a6c6b
Corey <reg@pathrider.ca>
2023-01-02 20:07:47 -0700

    Refactoring

--- Common/Utils/ConcurrentList.cs
+++ Common/Utils/ConcurrentList.cs
@@ -4,71 +4,85 @@ using System.Threading;
 
 namespace OLabWebAPI.Utils
 {
-    public class ConcurrentList<T>
+  public class ConcurrentList<T>
+  {
+    private readonly IList<T> _items = new List<T>();
+    private readonly ILogger _logger;
+    private static readonly Mutex mutex = new Mutex();
+
+    public ConcurrentList(ILogger logger)
     {
-        private readonly IList<T> _items = new List<T>();
-        private readonly ILogger _logger;
-        private static readonly Mutex mutex = new Mutex();
+      _logger = logger;
+    }
 
-        public ConcurrentList(ILogger logger)
-        {
-            _logger = logger;
-        }
+    public void Lock()
+    {
+      mutex.WaitOne();
+    }
 
-        public void Lock()
-        {
-            mutex.WaitOne();
-        }
+    public void Unlock()
+    {
+      mutex.ReleaseMutex();
+    }
 
-        public void Unlock()
-        {
-            mutex.ReleaseMutex();
-        }
+    public IList<T> Items
+    {
+      get { return _items; }
+    }
 
-        public IList<T> Items
-        {
-            get { return _items; }
-        }
-
-        public int Count
-        {
-            get
-            {
-                int count = 0;
-                mutex.WaitOne();
-                count = _items.Count;
-                mutex.ReleaseMutex();
-                return count;
-            }
-
-        }
-
-        public int Add(T item)
-        {
-            int count = 0;
-
-            mutex.WaitOne();
-            _items.Add(item);
-            count = _items.Count;
-            mutex.ReleaseMutex();
-
-            return count;
-        }
-
-        public T this[int index]
-        {
-            get { return GetAt(index); }
-        }
-
-        public T GetAt(int index)
-        {
-            T item;
-            mutex.WaitOne();
-            item = _items[index];
-            mutex.ReleaseMutex();
-
-            return item;
-        }
+    public int Count
+    {
+      get
+      {
+        int count = 0;
+        mutex.WaitOne();
+        count = _items.Count;
+        mutex.ReleaseMutex();
+        return count;
+      }
 
     }
+
+    public void Remove(int index)
+    {
+      mutex.WaitOne();
+      _items.RemoveAt(index);
+      mutex.ReleaseMutex();
+    }
+
+    public void Remove(T item)
+    {
+      mutex.WaitOne();
+      _items.Remove(item);
+      mutex.ReleaseMutex();
+    }
+
+    public int Add(T item)
+    {
+      int count = 0;
+
+      mutex.WaitOne();
+      _items.Add(item);
+      count = _items.Count;
+      mutex.ReleaseMutex();
+
+      return count;
+    }
+
+    public T this[int index]
+    {
+      get { return GetAt(index); }
+    }
+
+    public T GetAt(int index)
+    {
+      T item;
+      mutex.WaitOne();
+      item = _items[index];
+      mutex.ReleaseMutex();
+
+      return item;
+    }
+
+  }
 }
