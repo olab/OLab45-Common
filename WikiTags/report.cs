0c4ad95b087a91d6d88fc16fe4325a702818271b
Corey <reg@pathrider.ca>
2023-02-02 12:28:30 -0700

    Added support for OlabReportTag

---
+++ WikiTags/report.cs
@@ -0,0 +1,10 @@
+﻿using OLabWebAPI.Common;
+using OLabWebAPI.Utils;
+
+[WikiTagModule("REPORT")]
+public class ReportWikiTag : WikiTag0Argument
+{
+  public ReportWikiTag(OLabLogger logger) : base(logger, "OlabReportTag")
+  {
+  }
+}
