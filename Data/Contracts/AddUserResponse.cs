81971c83724b149c6834226e01c69f5ed3cdba8b
Corey <reg@pathrider.ca>
2023-02-08 21:01:14 -0700

    Added std response

--- Data/Contracts/AddUserResponse.cs
+++ Data/Contracts/AddUserResponse.cs
@@ -1,8 +1,14 @@
 namespace OLabWebAPI.Model
 {
-    public class AddUserResponse
+  public class AddUserResponse
+  {
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Message { get; set; }
+
+    public AddUserResponse()
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+      Message = "OK";
     }
+  }
 }
