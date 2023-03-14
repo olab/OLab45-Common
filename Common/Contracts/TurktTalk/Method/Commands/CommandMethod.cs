d6877dc5c8dca5093ba2fc0169bdb88d81cd577d
Corey <reg@pathrider.ca>
2023-03-14 07:59:19 -0600

    Moved turktalk contracts to Common

--- Common/Contracts/TurktTalk/Method/Commands/CommandMethod.cs
+++ Common/Contracts/TurktTalk/Method/Commands/CommandMethod.cs
@@ -4,23 +4,23 @@ using Newtonsoft.Json.Linq;
 
 namespace OLabWebAPI.TurkTalk.Methods
 {
-    /// <summary>
-    /// Defines a command method
-    /// </summary>
-    public abstract class CommandMethod : Method
+  /// <summary>
+  /// Defines a command method
+  /// </summary>
+  public abstract class CommandMethod : Method
+  {
+    public string Command { get; set; }
+
+    public CommandMethod(string recipientGroupName, string command) : base(recipientGroupName, "Command")
     {
-        public string Command { get; set; }
-
-        public CommandMethod(string recipientGroupName, string command) : base(recipientGroupName, "Command")
-        {
-            Guard.Argument(command).NotEmpty(command);
-            Command = command;
-        }
-
-        public override string ToJson()
-        {
-            var rawJson = System.Text.Json.JsonSerializer.Serialize(this);
-            return JToken.Parse(rawJson).ToString(Formatting.Indented);
-        }
+      Guard.Argument(command).NotEmpty(command);
+      Command = command;
     }
+
+    public override string ToJson()
+    {
+      var rawJson = System.Text.Json.JsonSerializer.Serialize(this);
+      return JToken.Parse(rawJson).ToString(Formatting.Indented);
+    }
+  }
 }
