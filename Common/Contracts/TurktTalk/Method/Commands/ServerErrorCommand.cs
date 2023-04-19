cd1ce48da8f1199ed39b23f7c82f69f1a0c2d49c
Corey <reg@pathrider.ca>
2023-04-19 12:40:11 -0600

    Added moderator login fence

---
+++ Common/Contracts/TurktTalk/Method/Commands/ServerErrorCommand.cs
@@ -0,0 +1,17 @@
+﻿using OLabWebAPI.TurkTalk.BusinessObjects;
+using OLabWebAPI.Common.Contracts;
+using System.Collections.Generic;
+using OLabWebAPI.TurkTalk.Methods;
+
+namespace OLabWebAPI.TurkTalk.Commands
+{
+    public class ServerErrorCommand : CommandMethod
+    {
+        public string Data { get; set; }
+
+        public ServerErrorCommand(string connectionId, string message) : base(connectionId, "servererror")
+        {
+          Data = message;
+        }
+    }
+}
