30b5807a7a235097d2b83e18a5004317cf3d384d
corey wirun <corey@cardinalcreek.ca>
2024-03-04 15:17:05 -0700

    unit test updates

---
+++ Common/Contracts/HealthResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLab.Common.Contracts;
+public class HealthResult
+{
+  public HttpStatusCode statusCode;
+  public System.Version main;
+  public object modules;
+  public string message;
+}
