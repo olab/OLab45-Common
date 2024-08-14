1c43ae0f156cd9f342a94f975538d5b169513e7a
Corey Wirun <reg@pathrider.ca>
2024-08-14 12:32:19 -0600

    UserMapper change

--- Data/Mappers/UsersMapper.cs
+++ Data/Mappers/UsersMapper.cs
@@ -3,6 +3,7 @@ using OLab.Api.Model;
 using OLab.Api.ObjectMapper;
 using OLab.Common.Interfaces;
 using OLab.Data.Dtos;
+using System.Linq;
 
 namespace OLab.Data.Mappers;
 
@@ -12,6 +13,26 @@ public class UsersMapper : OLabMapper<Users, UsersDto>
     IOLabLogger logger,
     OLabDBContext dbContext) : base(logger, dbContext)
   {
+  }
+
+  /// <summary>
+  /// Convert a physical object to a specific dto. 
+  /// </summary>
+  /// <remarks>
+  /// Allows for derived class specific overrides that 
+  /// don't fit well with default implementation
+  /// </remarks>
+  /// <param name="phys">Physical object</param>
+  /// <param name="source">Base dto object</param>
+  /// <returns>Dto object</returns>
+  public override UsersDto PhysicalToDto(Users phys, UsersDto source)
+  {
+    var dto = base.PhysicalToDto(phys, source);
+
+    dto.Roles = 
+      new UserGroupRolesMapper( GetLogger(), GetDbContext() ).PhysicalToDto( phys.UserGrouproles.ToList() );
+
+    return dto;
   }
 
 }
