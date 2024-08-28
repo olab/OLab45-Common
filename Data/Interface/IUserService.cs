d18182ffafd906b28d3cba21cd024248625f36b0
Corey Wirun <reg@pathrider.ca>
2024-08-28 13:38:48 -0600

    "stylesheet

--- Data/Interface/IUserService.cs
+++ Data/Interface/IUserService.cs
@@ -11,15 +11,13 @@ public interface IUserService
   //Users Authenticate(LoginRequest model);
   void ChangePassword(Users user, ChangePasswordRequest model);
 
-  Task<List<Users>> AddUsersAsync(List<AddUserRequest> items);  
+  Task<List<UsersDto>> AddUsersAsync(List<AddUserRequest> items);  
   Task<List<AddUserResponse>> DeleteUsersAsync(List<DeleteUsersRequest> items);
-  Task<Users> AddUserAsync(AddUserRequest item);
-  Task<Users> EditUserAsync(AddUserRequest item);
+  Task<UsersDto> AddUserAsync(AddUserRequest item);
+  Task<UsersDto> EditUserAsync(AddUserRequest item);
   Task<AddUserResponse> GetUsersAsync(AddUserRequest item);
   IList<UsersDto> GetUsers(string userName);
 
-
-  IEnumerable<Users> GetAll();
   Users GetById(uint? id);
   Users GetByUserName(string userName);
 }
