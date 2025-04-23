43c618a6584ec7e2461c8a965ada205a5116f9a3
Corey Wirun <RotateAt60MPH@users.noreply.github.com>
2025-04-23 14:23:32 -0600

    fixed NumTries mapping

--- Import/OLab3/Dtos/XmlMapQuestionDto.cs
+++ Import/OLab3/Dtos/XmlMapQuestionDto.cs
@@ -54,7 +54,7 @@ public class XmlMapQuestionDto : XmlImportDto<XmlMapQuestions>
       item.CounterId = counterDto.GetIdTranslation( GetFileName(), item.CounterId.Value );
 
     // special case for NumTries
-    if ( item.EntryTypeId == 4 && item.NumTries == 1 )
+    if ( item.EntryTypeId == 3 && item.NumTries == 2 )
       item.NumTries = -1;
 
     GetDbContext().SystemQuestions.Add( item );
