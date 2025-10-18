01f00d8def3712dfc42f7d4277d7cfbd06403b93
Corey Wirun <RotateAt60MPH@users.noreply.github.com>
2025-10-18 16:18:49 -0600

    rebuild for new kernel

--- Data/BusinessObjectsEx/RefreshToken.cs
+++ Data/BusinessObjectsEx/RefreshToken.cs
@@ -1,14 +1,22 @@
+using Newtonsoft.Json;
 using System;
 
 namespace OLab.Api.Model;
 
 public class RefreshToken
 {
+  [JsonProperty( "token" )]
   public string Token { get; set; }
+  [JsonProperty( "refresh" )]
   public string Refresh { get; set; }
+  [JsonProperty( "expires" )]
   public DateTime Expires { get; set; }
+  [JsonProperty( "isExpired" )]
   public bool IsExpired => DateTime.UtcNow >= Expires;
+  [JsonProperty( "created" )]
   public DateTime Created { get; set; }
+  [JsonProperty( "revoked" )]
   public DateTime? Revoked { get; set; }
+  [JsonProperty( "isActive" )]
   public bool IsActive => Revoked == null && !IsExpired;
 }
--- Data/Contracts/AuthenticateResponse.cs
+++ Data/Contracts/AuthenticateResponse.cs
@@ -1,13 +1,19 @@
+using Newtonsoft.Json;
 using System;
 
 namespace OLab.Api.Model;
 
 public class AuthenticateResponse
 {
+  [JsonProperty( "createdAt" )]
   public DateTime CreatedAt { get; set; }
+  [JsonProperty( "userName" )]
   public string UserName { get; set; }
+  [JsonProperty( "role" )]
   public string Role { get; set; }
+  [JsonProperty( "authInfo" )]
   public RefreshToken AuthInfo { get; set; }
+  [JsonProperty( "courseName" )]
   public string CourseName { get; set; }
 
   public AuthenticateResponse()
